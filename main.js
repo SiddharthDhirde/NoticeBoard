b6c283d56042efab99a03ee12ddf583f71c10f79
Siddharth Dhirde <siddharthdhirde09@gmail.com>
2024-03-29 05:24:21 +0530

    checking out

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register('https://github.com/SiddharthDhirde/busNoticeBoard/blob/main/sw_cached_site.js')
+      .register('https://siddharthdhirde.github.io/busNoticeBoard/sw_cached_site.js')
       .then((reg) => console.log("ServiceWorker registered successfully"))
       .catch((err) => console.log(`ServiceWorker registration failed: ${err}`));
   });
