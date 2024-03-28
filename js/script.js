0fdc4a01e68c69be06acbc508ad1a364e78f6e7b
Siddharth Dhirde <siddharthdhirde09@gmail.com>
2024-03-29 00:08:07 +0530

    register service worker

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
-// Set the localNotice in the localStorage
-var localNotice = "This is a Local notice from the server!";
-localStorage.setItem('notice', localNotice);
+// Make sure that service worker are supported
 
-// Update the content of the notice-text paragraph element
-var noticeTextElement = document.getElementById('notice-text');
-noticeTextElement.textContent = localNotice;
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("../sw_cached_pages.js")
+      .then(reg => console.log("ServiceWorker Registered Successfully"))
+      .catch(err => console.log("ServiceWorker registration failed: ", err));
+  });
+}
+
+// // Set the localNotice in the localStorage
+// var localNotice = "Dont have a Good Day, Have a Great Day!";
+// localStorage.setItem("notice", localNotice);
+
+// // Update the content of the notice-text paragraph element
+// var noticeTextElement = document.getElementById("notice-text");
+// noticeTextElement.textContent = localNotice;
