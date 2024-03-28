64f2dc7d4328b4fd1fe31dbf8884eaa6b1f66465
Siddharth Dhirde <siddharthdhirde09@gmail.com>
2024-03-29 05:19:19 +0530

    checking out

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register('/sw_cached_site.js')
+      .register('./sw_cached_site.js')
       .then((reg) => console.log("ServiceWorker registered successfully"))
       .catch((err) => console.log(`ServiceWorker registration failed: ${err}`));
   });
