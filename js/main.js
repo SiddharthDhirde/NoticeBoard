1057d26d47cd9b3efefa05cdcc94d6fab278df28
Siddharth Dhirde <siddharthdhirde09@gmail.com>
2024-03-30 22:32:33 +0530

    add logic for working with notices.json instead of working with array

--- js/main.js
+++ js/main.js
@@ -2,43 +2,37 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register('https://siddharthdhirde.github.io/NoticeBoard/sw_cached_site.js')
+      .register(
+        "https://siddharthdhirde.github.io/NoticeBoard/sw_cached_site.js"
+      )
       .then((reg) => console.log("ServiceWorker registered successfully"))
       .catch((err) => console.log(`ServiceWorker registration failed: ${err}`));
   });
 }
 
+function fetchNoticeMessages() {
+  return fetch("../notices.json")
+    .then((response) => response.json())
+    .then((data) => data.map((item) => item.message));
+}
 
-// Define array for notice messages
-var noticeMessages = [
-  'Don\'t have a Good Day, Have a Great Day!',
-  '"All I ask is that TODAY, YOU do the BEST WORK of your entire life" - Steve Jobs',
-  '"Imagination is more important than knowledge. For knowledge is limited, whereas imagination embraces the entire world, stimulating progress, giving birth to evolution." - Albert Einstein',
-  '"The only way to do great work is to love what you do." - Steve Jobs',
-  '"If you want to find the secrets of the universe, think in terms of energy, frequency, and vibration." - Nikola Tesla',
-  '"The only way to get started is to quit talking and begin doing." - Walt Disney',
-  '"The best way to predict the future is to create it." - Peter Drucker',
-  '"The only limit to our realization of tomorrow will be our doubts of today." - Franklin D. Roosevelt',
-  '"The true sign of intelligence is not knowledge but imagination." - Albert Einstein',
-  '"The day science begins to study non-physical phenomena, it will make more progress in one decade than in all the previous centuries of its existence." - Nikola Tesla',
-  '"The only thing standing between you and your goal is the story you keep telling yourself as to why you can\'t achieve it." - Jordan Belfort',
-  
-
-];
-
-// Initialize index to track current notice message
 var currentIndex = 0;
 
-// Function to update notice text
 function updateNoticeText() {
-  // Get the current notice message from the array
-  var currentNotice = noticeMessages[currentIndex];
+  fetchNoticeMessages()
+    .then((messages) => {
+      // Get the current notice message from the array
+      var currentNotice = messages[currentIndex];
 
-  // Update the notice text
-  document.getElementById("notice-text").textContent = currentNotice;
+      // Update the notice text
+      document.getElementById("notice-text").textContent = currentNotice;
 
-  // Increment index for the next notice message
-  currentIndex = (currentIndex + 1) % noticeMessages.length;
+      // Increment index for the next notice message
+      currentIndex = (currentIndex + 1) % messages.length;
+    })
+    .catch((error) => {
+      console.error("Error fetching notice messages:", error);
+    });
 }
 
 // Initial call to update notice text
